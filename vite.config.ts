928cd99f5964341d8cb15b5b314ac21dc993d0ea
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2025-11-01 14:33:21 +0000

    sfde

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nyakoi-Services/',   // must match repo name exactly
+  base: '/',   // must match repo name exactly
 })
 
 
